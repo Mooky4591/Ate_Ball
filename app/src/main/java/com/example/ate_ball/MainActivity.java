04717f05b0f3a4a25fc696c74c3c366d39506251
Scott <scottrobinson4591@gmail.com>
2020-12-24 18:08:41 -0500

    Added the spinner elements and worked on capturing the value

--- app/src/main/java/com/example/ate_ball/MainActivity.java
+++ app/src/main/java/com/example/ate_ball/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.ate_ball;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         Spinner distancespin;
         RadioButton $, $$, $$$, $$$$;
 
+
 //initialize each component
         findbttn = (Button) findViewById(R.id.find_bttn);
         distancespin = (Spinner) findViewById(R.id.distance);
@@ -26,6 +33,26 @@ public class MainActivity extends AppCompatActivity {
         $$$ = (RadioButton) findViewById(R.id.three_dollars);
         $$$$ = (RadioButton) findViewById(R.id.four_dollars);
 
+//creating the spinner elements
+        ArrayList<String> list = new ArrayList<>();
+        list.add("5 miles");
+        list.add("10 miles");
+        list.add("15 miles");
+        list.add("20 miles");
 
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        distancespin.setAdapter(arrayAdapter);
+        distancespin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String tutorialsName = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), "Selected: " + tutorialsName, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView <?> parent) {
+            }
+        });
     }
 }
