26c60969d2698d41e8b7ad54d1265df18480e9a5
Scott <scottrobinson4591@gmail.com>
2020-12-24 17:20:45 -0500

    Created the main menu UI

--- app/src/main/java/com/example/ate_ball/MainActivity.java
+++ app/src/main/java/com/example/ate_ball/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.ate_ball;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button findbttn;
+        Spinner distancespin;
+        RadioButton $, $$, $$$, $$$$;
+
+
+        findbttn = (Button) findViewById(R.id.find_bttn);
+        distancespin = (Spinner) findViewById(R.id.distance);
+        $ = (RadioButton) findViewById(R.id.one_dollar);
+        $$ = (RadioButton) findViewById(R.id.two_dollars);
+        $$$ = (RadioButton) findViewById(R.id.three_dollars);
+        $$$$ = (RadioButton) findViewById(R.id.four_dollars);
+
+
     }
 }
